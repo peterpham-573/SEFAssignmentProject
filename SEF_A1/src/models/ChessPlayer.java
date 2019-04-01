2bdbd0eea9de3de49cef4b716a1348d54ad8f60f
Peter <s3717828@student.rmit.edu.au>
2019-04-01 13:21:40 +1100

    Testing Commit

---
+++ SEF_A1/src/models/ChessPlayer.java
@@ -0,0 +1,29 @@
+package models;
+
+import models.interfaces.Player;
+
+public class ChessPlayer implements Player
+{
+	private String playerName;
+
+	@Override
+	public String getPlayerName() {
+		return playerName;
+		//yeet
+	}
+
+	@Override
+	public int getPoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPoints(int points) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//testies 123
+
+}
---
+++ SEF_A1/src/models/interfaces/Player.java
@@ -0,0 +1,19 @@
+package models.interfaces;
+
+/*
+ * 				SEF PROJECT - DAVID VO, DANNY LE, PETEY DERASMO, PETER PHAM
+ * 
+ * 		class will be the players for the chess game, will most likely include 
+ * 		
+ */
+
+public interface Player {
+
+	   public abstract String getPlayerName();
+	   
+	   public abstract int getPoints();
+	   
+	   public abstract void setPoints(int points);
+	   
+	   
+}
