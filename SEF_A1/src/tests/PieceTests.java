50417a21acb9bb5dd51408f69881ba81657a7745
Peter <s3680233@student.rmit.edu.au>
2019-04-07 17:17:56 +1000

    Updated Some tests

--- SEF_A1/src/tests/PieceTests.java
+++ SEF_A1/src/tests/PieceTests.java
@@ -9,29 +9,46 @@ import org.junit.jupiter.api.Test;
 
 import models.ChessBoard;
 import models.PiecePosition;
+import models.pieces.Bishop2;
+import models.pieces.Knight2;
 import models.pieces.Rook2;
 
 class PieceTests {
 	
 	ChessBoard chessBoard;
 	Rook2 rook2;
+	Knight2 knight2;
+	Bishop2 bishop2;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		chessBoard = new ChessBoard();
 		rook2 = new Rook2(false, new PiecePosition(0, 0));
+		knight2 = new Knight2(false, new PiecePosition(0, 0));
+		bishop2 = new Bishop2(false, new PiecePosition(0, 0));
 	}
 
 	@Test
 	void rookMovementTest() {
 		LinkedList<PiecePosition> piecePositions;
 		piecePositions = rook2.movement();
-//		for (PiecePosition piecePosition : piecePositions) {
-//			System.out.println(piecePosition.getCol() + piecePosition.getRow());
-//		}
-		assertEquals(4, piecePositions.size()); // This will have to change after further implementation of Movement
-		
-		
+		// This will have to change after further implementation of Movement
+		assertEquals(4, piecePositions.size()); 
 	}
 
+	@Test
+	void knightMovementTest() {
+		LinkedList<PiecePosition> piecePositions;
+		piecePositions = knight2.movement();
+		// This will have to change after further implementation of Movement
+		assertEquals(2, piecePositions.size()); 
+	}
+	
+	@Test
+	void bishopMovementTest() {
+		LinkedList<PiecePosition> piecePositions;
+		piecePositions = bishop2.movement();
+		// This will have to change after further implementation of Movement
+		assertEquals(2, piecePositions.size()); 
+	}
 }
