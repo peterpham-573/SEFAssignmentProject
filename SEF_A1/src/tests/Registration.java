4c73be0f79f6c91830ed2c5ee30a922bbcf9e0ed
s3722067 <41456164+s3722067@users.noreply.github.com>
2019-04-14 14:36:32 +1000

    Update Registration.java

--- SEF_A1/src/tests/Registration.java
+++ SEF_A1/src/tests/Registration.java
@@ -1,6 +1,12 @@
 package tests;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,36 +16,78 @@ import models.ChessPlayer;
 
 class Registration {
 	ChessGameRegistry chessGameRegistry;
+	//chessGameRegistry.playerLogin(Player player)
+	//Returns a int which represents the possible outcomes of trying to log in a player
+	//int = 0. Player 1 was added to the LoginArray (Attempt to login with valid login ID and password succeeds).
+	//int = 1. Attempt to login	with the same valid	ID and login fails. 
+	//int = 2. Player 2 was added to the LoginArray (Attempt to login with valid login ID and password succeeds).
+	//int = 3. Attempt to login	with invalid login ID and password fails.
 	@BeforeEach
 	void setUp() throws Exception {
 		chessGameRegistry = new ChessGameRegistry();
+		chessGameRegistry.addPlayerToRegistration(new ChessPlayer("TestingPlayer", "Hello", "World"));
+		chessGameRegistry.addPlayerToRegistration(new ChessPlayer("Danny", "Danny", "Le"));
 	}
 
 	@AfterEach
-	void tearDown() throws Exception {
+	void tearDownEach() throws Exception {
+		//Deletes the two players created after each tests
+		final int PLAYERS_CREATED = 2;
+		deletePlayers(PLAYERS_CREATED);
+	}
+
+	
+	@AfterAll
+	public static void tearDown() throws Exception {
+		//Deletes the two players created after all the tests are run
+		final int PLAYERS_CREATED = 2;
+		deletePlayers(PLAYERS_CREATED);
 	}
 	
-	@Test
+	private static void deletePlayers(int PLAYERS_CREATED) throws Exception {
+		//Deletes any players that the tests may have created as part of the tests
+		for(int i = 0; i < PLAYERS_CREATED;i++ ) {
+		StringBuilder StringBuilder = new StringBuilder();
+		File Data = new File("src/Registrations.txt");
+		Scanner scan = null;
+		BufferedWriter fWriter = null;
+		scan = new Scanner(Data);
+		
+	    while (scan.hasNextLine()) {
+	        String line = scan.nextLine();
+	        if (scan.hasNextLine()) {
+	            StringBuilder.append(line+ "\n");
+	        }
+	    }
+	    
+	    fWriter = new BufferedWriter(new FileWriter("src/Registrations.txt"));
+	    fWriter.write(StringBuilder.toString());
+	    fWriter.close();
+		}
+	}
+
+	
 	//Login a player inside the registry
-	void registerPlayer() {
+	@Test
+	void loginOnePlayer() {
 		String[] playerDetails = {"Hello" ,"World"};
 		ChessPlayer newPlayer = chessGameRegistry.getPlayer(playerDetails);
 		assertEquals(chessGameRegistry.playerLogin(newPlayer),0);
 	}
 	
-	@Test
 	//Login two players with the same login details
-	void registerPlayer2() {
+	@Test
+	void loginTwoPlayersSameDetails() {
 		String[] playerDetails = {"Hello" ,"World"};
 		ChessPlayer newPlayer = chessGameRegistry.getPlayer(playerDetails);
 		assertEquals(chessGameRegistry.playerLogin(newPlayer),0);
 		assertEquals(chessGameRegistry.playerLogin(newPlayer),1);
 	}
 	
-	@Test
-	void registerPlayer3() {
 	//Login a player inside the registry
 	//Login a player that doesn't exist inside the registry	
+	@Test
+	void loginTwoPlayersOneFakePlayer() {
 		String[] playerDetails = {"Hello" ,"World"};
 		String[] playerDetails2 = {"FAKE" ,"PLAYER"};
 		ChessPlayer newPlayer = chessGameRegistry.getPlayer(playerDetails);
@@ -48,9 +96,9 @@ class Registration {
 		assertEquals(chessGameRegistry.playerLogin(newPlayer2),3);
 	}
 	
-	@Test
-	void registerPlayer4() {
 	//Login two different players inside the registry
+	@Test
+	void loginTwoDifferentPlayers() {
 		String[] playerDetails = {"Hello" ,"World"};
 		String[] playerDetails2 = {"Danny" ,"Le"};
 		ChessPlayer newPlayer = chessGameRegistry.getPlayer(playerDetails);
@@ -59,5 +107,36 @@ class Registration {
 		assertEquals(chessGameRegistry.playerLogin(newPlayer2),2);
 	}
 	
-
+	//Checks if a player of the same username is already inside the registry
+	@Test
+	void registerOnePlayerTwice() {
+		ChessPlayer newPlayer = new ChessPlayer("newPlayer", "New", "Player");
+		
+		//Checks if the player already exists in the registry
+		assertEquals(chessGameRegistry.userNameCheck(newPlayer),false);	
+		
+		//Player should be added to the registry as the player does not exist
+		if(!chessGameRegistry.userNameCheck(newPlayer)) {
+			chessGameRegistry.addPlayerToRegistration(newPlayer);
+		}
+		
+		//Checks if the player already exists in the registry
+		assertEquals(chessGameRegistry.userNameCheck(newPlayer),true);		
+		
+		//Player does not get added a second time
+		if(!chessGameRegistry.userNameCheck(newPlayer)) {
+			chessGameRegistry.addPlayerToRegistration(newPlayer);
+		}	
+	}
+	
+	//Login a player that does exist in the registry
+	//adds the player into the registry
+	//Attempts to login the same player
+	@Test
+	void loginThenRegisterPlayer() {
+		ChessPlayer newPlayer = new ChessPlayer("newPlayer", "New2", "Player2");
+		assertEquals(chessGameRegistry.playerLogin(newPlayer),3);
+		chessGameRegistry.addPlayerToRegistration(newPlayer);
+		assertEquals(chessGameRegistry.playerLogin(newPlayer),0);
+	}
 }
