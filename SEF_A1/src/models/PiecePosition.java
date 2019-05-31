a74fce3d8749a40481514c2b73f71d09a6eea6a9
Peter <s3717828@student.rmit.edu.au>
2019-05-31 14:43:07 +1000

    unused methods

--- SEF_A1/src/models/PiecePosition.java
+++ SEF_A1/src/models/PiecePosition.java
@@ -5,20 +5,6 @@ package models;
 
 
 public class PiecePosition {
-	
-	public class ColumnExeption extends Exception {
-		private static final long serialVersionUID = 1L;
-		String letter;
-		
-		public ColumnExeption(String s) {
-			letter = s;
-		}
-		
-		public String toString() {
-			return "Column Letter identifier " + letter + " is invalid";
-		}
-
-	}
 
 
 	private int row;
@@ -27,15 +13,6 @@ public class PiecePosition {
 	public PiecePosition(int row, int col) {
 		this.row = row;
 		this.col = col;
-	}
-	
-	public PiecePosition(int row, String col) {
-		this.row = row;
-		try {
-			this.col = colNameLetterToNumber(col);
-		} catch (ColumnExeption e) {
-			System.out.println(e.toString());
-		}
 	}
 	
 	public int getRow() {
@@ -48,32 +25,6 @@ public class PiecePosition {
 	
 	public boolean isEqual(PiecePosition pos) {
 		return ((col == pos.getCol()) && (row == pos.getRow()));
-	}
-
-	/*
-	 * This is for converting the Character/String input of the naming convention
-	 * to the equivalent integer value for Col position in the game board
-	 * 
-	 * Should create an exception class to be thrown here for wrong input of col
-	 */
-	public int colNameLetterToNumber(String A_F) throws ColumnExeption {
-		String string = A_F.toUpperCase();
-		switch (string) {
-		case "A":
-			return 0;
-		case "B":
-			return 1;
-		case "C":
-			return 2;
-		case "D":
-			return 3;
-		case "E":
-			return 4;
-		case "F":
-			return 5;
-
-		}
-		throw new ColumnExeption(A_F);
 	}
 }
 
